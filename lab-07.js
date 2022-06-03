ed8fb00cd8f41ad728be09067aff0bc0d4f026b9
hsacrey <102761615+hsacrey@users.noreply.github.com>
2022-06-03 00:27:13 -0700

    added lab 7

---
+++ lab-07.js
@@ -0,0 +1,40 @@
+/*
+    
+    CIT 281: Lab 07
+    Hunter Sacrey
+
+*/
+
+class CustomError extends Error{
+    
+}
+throwGenericError = () => {
+    throw new Error("Generic error");
+}
+throwCustomError = () => {
+    throw new CustomError("Custom Error");
+}
+console.log("Force generic error")
+    try{
+        throwGenericError();
+        console.log("Generic Error try block");
+    }
+    catch(err){
+        console.log(err + " ");
+        console.log("Generic Error catch block");
+    }
+    finally{
+        console.log("Generic Error finally block");
+    }
+    console.log("Force custom error")
+    try{
+        throwCustomError();
+        console.log("Custom Error try block");
+    }
+    catch(err){
+        console.log(err + " ");
+        console.log("Custom Error catch block");
+    }
+    finally{
+        console.log("Custom Error finally block");
+    }
